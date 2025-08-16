7f89964484fca7cf79eda9ad7f3f933a13d5001c
m-moeez123 <moeezmazhar372@gmail.com>
2025-08-16 20:42:18 +0500

    Update APP.py

--- APP.py
+++ APP.py
@@ -86,7 +86,7 @@ cluster_placeholder = st.empty()
 import streamlit as st
 import pandas as pd
 import numpy as np
-#import joblib
+import joblib
 import os
 import shap
 import matplotlib.pyplot as plt
@@ -209,4 +209,5 @@ recommendations_placeholder = st.empty()
 st.subheader("Patient Phenotype Cluster")
 
 cluster_placeholder = st.empty()
+
 
